33330cd78500193b54d49900a5fa40389eb1392c
Mitun Suresh <116903938+Mitun-at-work@users.noreply.github.com>
2022-11-27 10:47:18 +0530

    Update hydrationcontainer.dart

--- widgets/hydrationcontainer.dart
+++ widgets/hydrationcontainer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fooddiets/globals/theme.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class HydrationContainer extends StatelessWidget {
   const HydrationContainer({super.key});
@@ -8,11 +9,11 @@ class HydrationContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: bgblue,
-      height: 180,
+      height: 250,
       width: 180,
       child: Padding(
         padding: const EdgeInsets.symmetric(
-          horizontal: 25,
+          horizontal: 15,
           vertical: 10,
         ),
         child: Column(
@@ -23,6 +24,7 @@ class HydrationContainer extends StatelessWidget {
                 Text(
                   "Water",
                   style: TextStyle(
+                    fontWeight: FontWeight.bold,
                     color: bluetext,
                     fontSize: 20,
                   ),
@@ -33,6 +35,56 @@ class HydrationContainer extends StatelessWidget {
                 ),
               ],
             ),
+            Container(
+              margin: const EdgeInsets.only(
+                top: 15,
+              ),
+              child: CircularPercentIndicator(
+                radius: 60.0,
+                animation: true,
+                animationDuration: 1500,
+                lineWidth: 4.0,
+                percent: .1,
+                center: const Text(
+                  "2 Litres",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: bluetext,
+                    fontSize: 20.0,
+                  ),
+                ),
+                circularStrokeCap: CircularStrokeCap.butt,
+                backgroundColor: const Color.fromRGBO(
+                  31,
+                  91,
+                  113,
+                  1,
+                ),
+                progressColor: const Color.fromRGBO(
+                  194,
+                  216,
+                  237,
+                  1,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                top: 10,
+              ),
+              child: const Text(
+                "You are good",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromRGBO(
+                    31,
+                    91,
+                    113,
+                    1,
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
